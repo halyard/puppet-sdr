facd9aafcfabde68828dc4af46257e9a00c8a0be
Les Aker <me@lesaker.org>
2022-04-22 16:21:02 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+puppet-sdr
+===========
+
+[![Puppet Forge](https://img.shields.io/puppetforge/v/halyard/sdr.svg)](https://forge.puppetlabs.com/halyard/sdr)
+[![MIT Licensed](http://img.shields.io/badge/license-MIT-green.svg?style=flat)](https://tldrlegal.com/license/mit-license)
+
+Module to configure Software Defined Radio tools
+
+## Usage
+
+To install SDR tools:
+
+```puppet
+include sdr
+```
+
---
+++ manifests/init.pp
@@ -0,0 +1,26 @@
+# @summary Configure SDR tools
+#
+# @param rtl_433_version sets the tag of the rtl_433 repo to deploy
+class sdr (
+  String $rtl_433_version = '21.12',
+) {
+  package { [
+      'rtl-sdr',
+  ]: }
+
+  vcsrepo { '/opt/rtl_433':
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/merbanan/rtl_433.git',
+    revision => $rtl_433_version,
+  }
+
+  -> file { '/opt/rtl_433/build':
+    ensure => directory,
+  }
+
+  -> exec { 'cmake .. && make install':
+    cwd    => '/opt/rtl_433/build',
+    unless => '/usr/local/bin/rtl_433 -V 2>&1 | awk \'/rtl_433 version/ { print $3 }\'',
+  }
+}
